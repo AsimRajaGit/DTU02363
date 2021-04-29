3dc63aed15a7ffbe72fc1e7ebfcd909d657a0d99
Asim Raja <asim.raja11@gmail.com>
2021-04-30 00:49:33 +0200

    update

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,8 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
-import { BasketService } from 'src/app/basket/basket.service';
+import { CartService } from 'src/app/basket/basket.service';
 import { Observable } from 'rxjs';
-import { IBasket } from 'src/app/shared/models/basket';
+import { ICart } from 'src/app/shared/models/basket';
 import { IUser } from 'src/app/shared/models/user';
 import { AccountService } from 'src/app/account/account.service';
 
@@ -15,10 +15,10 @@ export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
   currentUser$: Observable<IUser>;
 
-  constructor(private basketService: BasketService, private accountService: AccountService) { }
+  constructor(private cartervice: cartService, private accountService: AccountService) { }
 
   ngOnInit() {
-    this.basket$ = this.basketService.basket$;
+    this.cart$ = this.cartService.basket$;
     this.currentUser$ = this.accountService.currentUser$;
   }
 
--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
-import { ProcutItemComponent } from './product-item/procut-item.component';
+import { ProcutItemComponent } from './product-item/product-item.component';
 
 
 
