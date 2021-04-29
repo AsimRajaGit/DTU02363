2ec5ddfd0d6ae2ea982d7b81dda1d927a68967a5
Asim Raja <asim.raja11@gmail.com>
2021-04-29 23:24:52 +0200

    core component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule
+    CoreModule
    ],
   providers: [],
   bootstrap: [AppComponent]
