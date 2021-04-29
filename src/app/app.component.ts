6430323f549419439707c95a2334bfa2c99ba92c
Asim Raja <asim.raja11@gmail.com>
2021-04-29 19:59:04 +0200

    list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'JaDes Design';
-  
+  products:any[];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-     this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
-       console.log(response);
+     this.http.get('http://localhost:5001/api/products').subscribe((response: any) => {
+       this.products = response.data;
       }, error => {
          console.log(error);
      });
