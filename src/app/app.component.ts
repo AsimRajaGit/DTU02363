768ddb316cd0013cfe40b4eabae1a249096e0504
Asim Raja <asim.raja11@gmail.com>
2021-04-29 19:31:07 +0200

    http first setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JaDes Design';
+  
+  constructor {private http: HttpClient} {}
+
+  ngOnInit{}: void {
+     this.http.get('http://localhost:5001/api/products').subscribe((response:any) => {
+       console function(error:any): void
+  },     error => {
+         console.log.{error};
+     });
 }
