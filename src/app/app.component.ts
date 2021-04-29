319c5df6fdaff655588071b0777eca49c4afe214
Asim Raja <asim.raja11@gmail.com>
2021-04-29 22:51:30 +0200

    models 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import {Iproduct} from './models/product';
+import {Ipagination} from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,7 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-     this.http.get('http://localhost:5001/api/products').subscribe((response: any) => {
+     this.http.get('https://localhost:5001/api/products').subscribe((response: Ipagination) => {
        this.products = response.data;
       }, error => {
          console.log(error);
